4ae104f9a114a4b6b1023676f67d55dcaf6e8156
mywulin <wangwulin@hotmail.com>
2020-06-14 23:26:47 +0800

    Update homework of week4

--- Week_04/L127_ladderLength.java
+++ Week_04/L127_ladderLength.java
@@ -1,7 +1,9 @@
 package Week4;
 
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class L127_ladderLength {
 	public static void main(String[] args) {
@@ -15,6 +17,41 @@ public class L127_ladderLength {
 
 	public int ladderLength(String beginWord, String endWord, List<String> wordList) {
 
+		int layer = 1;
+		Queue<String> queue = new LinkedList<String>();
+		queue.add(beginWord);
+		boolean[] visited = new boolean[wordList.size() + 1];
+		while (!queue.isEmpty()) {
+			layer++;
+			int size = queue.size();
+			while (size-- > 0) {
+				String cur = queue.poll();
+				for (int i = 0; i < wordList.size(); i++) {
+					if (visited[i])
+						continue;
+					String dic = wordList.get(i);
+					if (canChange(dic, cur)) {
+						if (dic.equals(endWord))
+							return layer;
+						queue.add(dic);
+						visited[i] = true;
+					}
+				}
+			}
+
+		}
+
+		return 0;
+	}
+
+	private boolean canChange(String dic, String cur) {
+		int diff = 0;
+		for (int i = 0; i < dic.length(); i++) {
+			if (dic.charAt(i) != cur.charAt(i)) {
+				diff++;
+			}
+		}
+		return diff == 1;
 	}
 
 }
